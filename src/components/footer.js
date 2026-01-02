583220a2a8a1e4d25a0e6329c867c7844e4d1cc5
Minimalist30 <minimalist0030@gmail.com>
2026-01-02 21:17:42 +0900

    feat: ライフセクションに画像を追加し、フッターにクレジット情報を追記

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,17 @@ const Footer = () => {
   return (
     <div className="text-center opacity-40 text-sm">
       &copy; {new Date().getFullYear()} Koki Nakagawa. All Rights Reserved.
+      <div className="mt-2 text-xs">
+        Based on{' '}
+        <a
+          href="https://www.craftz.dog/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          Takuya Matsuyama&apos;s website
+        </a>
+      </div>
     </div>
   )
 }
