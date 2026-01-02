19a908061a4086fddfe4baef08f3e32273f4f508
Minimalist30 <minimalist0030@gmail.com>
2026-01-02 20:03:35 +0900

    projectsの修正

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -34,7 +34,7 @@ const Projects = () => {
     return (
       <Layout title="Projects">
         <div>
-          <h3 className="text-xl mb-4 font-bold">研究室での活動</h3>
+          <h3 className="text-xl mb-4 font-bold">これまでの活動</h3>
           <div className="flex justify-center py-10">
             <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-teal-500"></div>
           </div>
