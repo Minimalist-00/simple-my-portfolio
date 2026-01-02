762c9a54592b41f5975c48636b2f0c58542c15bc
Minimalist30 <minimalist0030@gmail.com>
2026-01-02 23:48:12 +0900

    refactor: アイコンファイルをimagesディレクトリに整理し、プレビュー画像とメタ情報を更新

--- src/components/layouts/main.js
+++ src/components/layouts/main.js
@@ -9,34 +9,36 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Koki's homepage" />
         <meta name="author" content="Koki Nakagawa" />
-        <meta name="author" content="craftzdog" />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/images/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/images/favicon-16x16.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/images/apple-touch-icon.png"
         />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.ico"
+          type="image/x-icon"
+        />
         <meta name="twitter:title" content="Koki Nakagawa" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+
+        <meta name="twitter:image" content="/images/PreviewImage.png" />
         <meta property="og:site_name" content="Koki Nakagawa" />
         <meta name="og:title" content="Koki Nakagawa" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="/images/PreviewImage.png" />
         <title>Koki Nakagawa Portfolio</title>
       </Head>
 
